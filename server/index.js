73b283e2aafcb0612bb78daae3430ad505d6f159
shawn <shawnfeng610@gmail.com>
2023-03-11 00:25:01 +0800

    esp32 first commit and server/index.js add reset-arm

--- server/index.js
+++ server/index.js
@@ -14,37 +14,50 @@ app.use(cors());
 const esp32Ip = process.env.ESP32_IP;
 const esp32Port = process.env.ESP32_PORT;
 
-// 设置各轴的角度
+// 設置個軸的角度
 app.post('/set-axis-angle', (req, res) => {
-	const { axis, angle } = req.body;
-	axios
-		.get(
-			`http://${esp32Ip}:${esp32Port}/set-axis-angle?axis=${axis}&angle=${angle}`
-		)
-		.then((response) => {
-			res.send(response.data);
-		})
-		.catch((error) => {
-			console.log(error);
-			res.status(500).send({ error: 'Failed to set angle' });
-		});
+  const { axis, angle } = req.body;
+  axios
+    .get(
+      `http://${esp32Ip}:${esp32Port}/set-axis-angle?axis=${axis}&angle=${angle}`
+    )
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send({ error: 'Failed to set angle' });
+    });
 });
 
-// 获取各轴的角度
+// 獲取個軸的角度
 app.get('/get-angles', (req, res) => {
-	axios
-		.get(`http://${esp32Ip}:${esp32Port}/get-angles`)
-		.then((response) => {
-			res.send(response.data);
-		})
-		.catch((error) => {
-			console.log(error);
-			res.status(500).send({ error: 'Failed to get angles' });
-		});
+  axios
+    .get(`http://${esp32Ip}:${esp32Port}/get-angles`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send({ error: 'Failed to get angles' });
+    });
 });
 
-// 启动 Express 服务器
+// 重置機械手臂
+app.post('/reset-arm', (req, res) => {
+  axios
+    .get(`http://${esp32Ip}:${esp32Port}/reset-arm`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send({ error: 'Failed to reset arm' });
+    });
+});
+
+// 啟動 Express server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
