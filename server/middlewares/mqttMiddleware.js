bc7e458af6897570041957a381dc8e715c6fb26e
shawn <shawnfeng610@gmail.com>
2023-03-30 18:14:52 +0800

    server app structure

---
+++ server/middlewares/mqttMiddleware.js
@@ -0,0 +1,21 @@
+const mqtt = require('mqtt');
+const mqttClient = mqtt.connect('mqtt://localhost:1883');
+
+let currentAngles = {};
+
+mqttClient.on('connect', () => {
+  mqttClient.subscribe('esp32/angles');
+});
+
+mqttClient.on('message', (topic, message) => {
+  if (topic === 'esp32/angles') {
+    currentAngles = JSON.parse(message.toString());
+  }
+});
+
+const getCurrentAngles = () => currentAngles;
+
+module.exports = {
+  mqttClient,
+  getCurrentAngles,
+};
