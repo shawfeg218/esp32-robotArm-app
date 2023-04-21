bcf21a046426ef872d1c3b4c8573e39fc72554c5
shawn <shawnfeng610@gmail.com>
2023-04-22 04:38:53 +0800

    get esp32 status

--- webapp/src/components/arm-control/ArmControl.jsx
+++ webapp/src/components/arm-control/ArmControl.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import ArmControlView from './ArmControlView';
 
 export default function ArmControl() {
+  const [esp32Status, setEsp32Status] = useState({});
   const [targetAngles, setTargetAngles] = useState({
     A: 0,
     B: 0,
@@ -65,10 +66,21 @@ export default function ArmControl() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get('/api/get-esp32Status').then((res) => {
+        setEsp32Status(res.data);
+        console.log(res.data);
+      });
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <ArmControlView
       targetAngles={targetAngles}
       currentAngles={currentAngles}
+      esp32Status={esp32Status}
       handleChange={handleChange}
       handleReset={handleReset}
       handleCorrectAction={handleCorrectAction}
