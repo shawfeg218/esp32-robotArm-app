e0a8cd7efdc5bc6fbdfafd013d34ae9fbef04ecc
shawn <shawnfeng610@gmail.com>
2023-05-21 20:08:40 +0800

    navbar add connect status

--- webapp/src/pages/account/index.jsx
+++ webapp/src/pages/account/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import Account from '@/components/account/Account';
-import Esp32Devices from '@/components/arm-control/Esp32Devices';
+import ResetPassword from '@/components/account/ResetPassword';
 
 export default function AccountPage() {
   return (
     // <div className="flex-container">
-    //   <div>
-    //     <Account />
-    //   </div>
-    //   <div>
-    //     <Esp32Devices />
-    //   </div>
-    // </div>
-
     <Account />
+    // <ResetPassword />
+    // </div>
   );
 }
