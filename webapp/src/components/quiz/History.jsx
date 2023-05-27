dcbba35ebc77059d2805aee85ae8d2f5e2703c9b
shawn <shawnfeng610@gmail.com>
2023-05-28 00:45:46 +0800

    history table

--- webapp/src/components/quiz/History.jsx
+++ webapp/src/components/quiz/History.jsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from 'react';
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
-import Head from 'next/head';
-import $ from 'jquery';
-import 'datatables.net';
-import 'datatables.net-bs5';
+import React, { useEffect, useState } from 'react';
 import styles from '@/styles/History.module.css';
 
-const History = () => {
+export default function History() {
   const [recentHistory, setRecentHistory] = useState([]);
   const [page, setPage] = useState(0); // page starts from 0
   const [totalItems, setTotalItems] = useState(0);
@@ -35,12 +31,11 @@ const History = () => {
       .eq('user_id', user.id)
       .order('inserted_at', { ascending: false })
       .range(page * itemsPerPage, (page + 1) * itemsPerPage - 1); // fetch data for the current page
-
     if (error) {
       console.log('Error fetching recent history:', error);
     } else {
       // Fetch the names of the subjects associated with each entry
-      const dataWithSubjectName = await Promise.all(
+      const Data_with_subject_name = await Promise.all(
         data.map(async (entry) => {
           const { data: subjectData, error: subjectError } = await supabase
             .from('subjects')
@@ -57,7 +52,7 @@ const History = () => {
         })
       );
 
-      setRecentHistory(dataWithSubjectName);
+      setRecentHistory(Data_with_subject_name);
     }
   }
 
@@ -68,78 +63,59 @@ const History = () => {
   };
 
   const handleNextPage = () => {
+    // You might want to check if there are more data before incrementing the page
     if ((page + 1) * itemsPerPage < totalItems) {
       setPage(page + 1);
     }
   };
 
-  useEffect(() => {
-    $(document).ready(function () {
-      $('#example').DataTable();
-    });
-  }, []);
-
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css"
-        />
-      </Head>
-      <div className={styles.container}>
-        <h2>History</h2>
-        {recentHistory.length > 0 ? (
-          <table className="table table-striped table-bordered table-hover" id="example">
-            <thead>
-              <tr>
-                <th>科目</th>
-                <th>得分</th>
-                <th>Date</th>
+    <div className={styles.container}>
+      <h2>History</h2>
+      {recentHistory.length > 0 ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>科目</th>
+              <th>得分</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {recentHistory.map((entry, index) => (
+              <tr key={index}>
+                <td>{entry.subject_name}</td>
+                <td>{entry.score}</td>
+                <td>
+                  {new Date(entry.inserted_at).toLocaleDateString(undefined, {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                  })}{' '}
+                  {new Date(entry.inserted_at).toLocaleTimeString(undefined, {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  })}
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {recentHistory.map((entry, index) => (
-                <tr key={index}>
-                  <td>{entry.subject_name}</td>
-                  <td>{entry.score}</td>
-                  <td>
-                    {new Date(entry.inserted_at).toLocaleDateString(undefined, {
-                      year: 'numeric',
-                      month: '2-digit',
-                      day: '2-digit',
-                    })}{' '}
-                    {new Date(entry.inserted_at).toLocaleTimeString(undefined, {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>No history data available.</p>
-        )}
-        <div className={styles.btnContainer}>
-          <button onClick={handlePrevPage} disabled={page === 0}>
-            Prev
-          </button>
-          <p>{page + 1}</p>
-          <button onClick={handleNextPage} disabled={(page + 1) * itemsPerPage >= totalItems}>
-            Next
-          </button>
-        </div>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No history data available.</p>
+      )}
+      <div className={styles.btnContainer}>
+        <button onClick={handlePrevPage} disabled={page === 0}>
+          Prev
+        </button>
+        <p>{page + 1}</p>
+        <button
+          onClick={handleNextPage}
+          disabled={(page + 1) * itemsPerPage >= totalItems}
+        >
+          Next
+        </button>
       </div>
-      <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-      <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-      <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-    </>
+    </div>
   );
-};
-
-export default History;
+}
