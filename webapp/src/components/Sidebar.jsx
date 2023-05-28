039be307b86e5f0a8d86d08d609634a6b3ae75f1
shawn <shawnfeng610@gmail.com>
2023-05-28 21:24:13 +0800

    RWD sidebar and menuIcon

--- webapp/src/components/Sidebar.jsx
+++ webapp/src/components/Sidebar.jsx
@@ -6,46 +6,71 @@ import { TfiPanel } from 'react-icons/tfi';
 import { GrConnect } from 'react-icons/gr';
 import Link from 'next/link';
 import AppContext from '@/contexts/AppContext';
+import { useMediaQuery } from 'react-responsive';
+
+const Desktop = ({ children, setDisplaySidebar }) => {
+  const isDesktop = useMediaQuery({ minWidth: 1200 });
+  if (isDesktop) {
+    () => setDisplaySidebar(true);
+  }
+  return isDesktop ? children : null;
+};
 
 export default function Sidebar() {
   const { displaySidebar, setDisplaySidebar } = useContext(AppContext);
 
   return (
-    <div className={`${styles.container} ${displaySidebar ? styles.show : ''}`}>
-      <div className={styles.itemDiv}>
-        <Link href="/" passHref>
-          <div className={styles.item} onClick={() => setDisplaySidebar(false)}>
-            <div>
-              <AiOutlineHome className="reactIcons" size="2rem" />
+    <Desktop>
+      <div
+        className={`${styles.container} ${displaySidebar ? styles.show : ''}`}
+      >
+        <div className={styles.itemDiv}>
+          <Link href="/" passHref>
+            <div
+              className={styles.item}
+              onClick={() => setDisplaySidebar(false)}
+            >
+              <div>
+                <AiOutlineHome className="reactIcons" size="2rem" />
+              </div>
+              <p>HOME</p>
             </div>
-            <p>HOME</p>
-          </div>
-        </Link>
-        <Link href="/quiz" passHref>
-          <div className={styles.item} onClick={() => setDisplaySidebar(false)}>
-            <div>
-              <FaChalkboardTeacher className="reactIcons" size="2rem" />
+          </Link>
+          <Link href="/quiz" passHref>
+            <div
+              className={styles.item}
+              onClick={() => setDisplaySidebar(false)}
+            >
+              <div>
+                <FaChalkboardTeacher className="reactIcons" size="2rem" />
+              </div>
+              <p>QUIZ</p>
             </div>
-            <p>QUIZ</p>
-          </div>
-        </Link>
-        <Link href="/arm-control" passHref>
-          <div className={styles.item} onClick={() => setDisplaySidebar(false)}>
-            <div>
-              <TfiPanel className="reactIcons" size="2rem" />
+          </Link>
+          <Link href="/arm-control" passHref>
+            <div
+              className={styles.item}
+              onClick={() => setDisplaySidebar(false)}
+            >
+              <div>
+                <TfiPanel className="reactIcons" size="2rem" />
+              </div>
+              <p>操作手臂</p>
             </div>
-            <p>操作手臂</p>
-          </div>
-        </Link>
-        <Link href="/device" passHref>
-          <div className={styles.item} onClick={() => setDisplaySidebar(false)}>
-            <div>
-              <GrConnect className="reactIcons" size="2rem" />
+          </Link>
+          <Link href="/device" passHref>
+            <div
+              className={styles.item}
+              onClick={() => setDisplaySidebar(false)}
+            >
+              <div>
+                <GrConnect className="reactIcons" size="2rem" />
+              </div>
+              <p>連線設定</p>
             </div>
-            <p>連線設定</p>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Desktop>
   );
 }
--- webapp/src/pages/account/index.jsx
+++ webapp/src/pages/account/index.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import Account from '@/components/account/Account';
-import ResetPassword from '@/components/account/ResetPassword';
 
 export default function AccountPage() {
   return <Account />;
