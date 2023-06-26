477d2d5672d2d91b3b195694ea2db7339523a881
shawn <shawnfeng610@gmail.com>
2023-06-26 21:42:10 +0800

    reset-password button in Account

--- webapp/src/pages/_app.jsx
+++ webapp/src/pages/_app.jsx
@@ -3,26 +3,14 @@ import Layout from '@/components/Layout';
 import { AppContextProvider } from '@/contexts/AppContext';
 import nc from 'next-connect';
 import cors from 'cors';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider, useUser } from '@supabase/auth-helpers-react';
 import { NextUIProvider } from '@nextui-org/react';
-import { useRouter } from 'next/router';
 
 function App({ Component, pageProps }) {
   const handler = nc().use(cors());
   const [supabase] = useState(() => createBrowserSupabaseClient());
-  const { user } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) {
-      const { query } = router;
-      if (query.type === 'recovery' && query.token) {
-        router.push('/reset-password');
-      }
-    }
-  }, [user, router]);
 
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
