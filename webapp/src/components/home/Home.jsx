d9cf7e0eece84cc365e598c533c236c630da5b29
Arthur45440809 <Arthur45440809@gmail.com>
2023-06-26 22:42:21 +0800

    fix device update

--- webapp/src/components/home/Home.jsx
+++ webapp/src/components/home/Home.jsx
@@ -1,14 +1,14 @@
-import AppContext from '@/contexts/AppContext';
-import Link from 'next/link';
-import React, { useContext, useEffect, useState } from 'react';
-import { FaChalkboardTeacher } from 'react-icons/fa';
-import { GrConnect } from 'react-icons/gr';
-import { TfiPanel } from 'react-icons/tfi';
-import { BsMicFill } from 'react-icons/bs';
-import styles from '@/styles/Home.module.css';
-import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
-import History from '../quiz/History';
-import { Table, Loading, Button, Card, Text } from '@nextui-org/react';
+import AppContext from "@/contexts/AppContext";
+import Link from "next/link";
+import React, { useContext, useEffect, useState } from "react";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { GrConnect } from "react-icons/gr";
+import { TfiPanel } from "react-icons/tfi";
+import { BsMicFill } from "react-icons/bs";
+import styles from "@/styles/Home.module.css";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import History from "../quiz/History";
+import { Table, Loading, Button, Card, Text } from "@nextui-org/react";
 
 export default function Home() {
   const [recentHistory, setRecentHistory] = useState([]);
@@ -24,25 +24,25 @@ export default function Home() {
     setLoading(true);
 
     const { data, error } = await supabase
-      .from('result_history')
-      .select('*')
-      .eq('user_id', user.id)
-      .order('inserted_at', { ascending: false })
+      .from("result_history")
+      .select("*")
+      .eq("user_id", user.id)
+      .order("inserted_at", { ascending: false })
       .limit(3);
     if (error) {
-      console.log('Error fetching recent history:', error);
+      console.log("Error fetching recent history:", error);
     } else {
       // Fetch the names of the subjects associated with each entry
       const Data_with_subject_name = await Promise.all(
         data.map(async (entry) => {
           const { data: subjectData, error: subjectError } = await supabase
-            .from('subjects')
-            .select('name')
-            .eq('id', entry.subject_id)
+            .from("subjects")
+            .select("name")
+            .eq("id", entry.subject_id)
             .single();
 
           if (subjectError) {
-            console.log('Error fetching subject name:', subjectError);
+            console.log("Error fetching subject name:", subjectError);
           } else {
             // Add the subject name to the entry
             return { ...entry, subject_name: subjectData.name };
@@ -107,38 +107,55 @@ export default function Home() {
                     lined
                     shadow={false}
                     selectionMode="single"
-                    containerCss={{ width: '100%', minWidth: 'fit-content' }}
-                    css={{ height: 'auto', width: '100%' }}
+                    containerCss={{ width: "100%", minWidth: "fit-content" }}
+                    css={{ height: "auto", width: "100%" }}
                   >
                     <Table.Header>
-                      <Table.Column css={{ fontWeight: 'bold', fontSize: '$sm' }} align="center">
+                      <Table.Column
+                        css={{ fontWeight: "bold", fontSize: "$sm" }}
+                        align="center"
+                      >
                         科目
                       </Table.Column>
-                      <Table.Column css={{ fontWeight: 'bold', fontSize: '$sm' }} align="center">
+                      <Table.Column
+                        css={{ fontWeight: "bold", fontSize: "$sm" }}
+                        align="center"
+                      >
                         得分
                       </Table.Column>
-                      <Table.Column css={{ fontWeight: 'bold', fontSize: '$sm' }} align="center">
+                      <Table.Column
+                        css={{ fontWeight: "bold", fontSize: "$sm" }}
+                        align="center"
+                      >
                         Date
                       </Table.Column>
                     </Table.Header>
                     <Table.Body>
                       {recentHistory.map((entry, index) => (
                         <Table.Row key={index}>
-                          <Table.Cell css={{ textAlign: 'center' }}>
+                          <Table.Cell css={{ textAlign: "center" }}>
                             {entry.subject_name}
                           </Table.Cell>
-                          <Table.Cell css={{ textAlign: 'center' }}>{entry.score}</Table.Cell>
-                          <Table.Cell css={{ textAlign: 'center' }}>
-                            {new Date(entry.inserted_at).toLocaleDateString(undefined, {
-                              year: 'numeric',
-                              month: '2-digit',
-                              day: '2-digit',
-                            })}
+                          <Table.Cell css={{ textAlign: "center" }}>
+                            {entry.score}
+                          </Table.Cell>
+                          <Table.Cell css={{ textAlign: "center" }}>
+                            {new Date(entry.inserted_at).toLocaleDateString(
+                              undefined,
+                              {
+                                year: "numeric",
+                                month: "2-digit",
+                                day: "2-digit",
+                              }
+                            )}
 
-                            {new Date(entry.inserted_at).toLocaleTimeString(undefined, {
-                              hour: '2-digit',
-                              minute: '2-digit',
-                            })}
+                            {new Date(entry.inserted_at).toLocaleTimeString(
+                              undefined,
+                              {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                              }
+                            )}
                           </Table.Cell>
                         </Table.Row>
                       ))}
@@ -147,7 +164,7 @@ export default function Home() {
                   <div>
                     {recentHistory.length > 0 && (
                       <Link href="/history" passHref>
-                        <button className="mt-0 mb-4 rounded-xl border-0 bg-blue-600 w-full">
+                        <button className="text-white mt-0 mb-4 rounded-xl border-0 bg-blue-600 w-full">
                           更多
                         </button>
                       </Link>
