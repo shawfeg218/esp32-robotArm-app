b5c40e9b11ad9597a4419191058f126d7a3c429c
shawn <shawnfeng610@gmail.com>
2023-06-27 07:54:40 +0800

    add Toast component in Device page

--- webapp/src/components/arm-control/Esp32Devices.jsx
+++ webapp/src/components/arm-control/Esp32Devices.jsx
@@ -7,6 +7,7 @@ import { AiOutlineDelete } from 'react-icons/ai';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { GrConnect } from 'react-icons/gr';
 import { Input, Spacer, Button } from '@nextui-org/react';
+import Toast from '../Toast';
 
 export default function Esp32Devices() {
   const supabase = useSupabaseClient();
@@ -18,12 +19,24 @@ export default function Esp32Devices() {
   const [editingDeviceIndex, setEditingDeviceIndex] = useState(null);
   const [editDeviceName, setEditDeviceName] = useState('');
   const [editMacAddress, setEditMacAddress] = useState('');
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const [toastType, setToastType] = useState('');
   const { setConnecting, setConnectedMacAddress, setConnectedDeviceName } = useContext(AppContext);
 
   const handleConnect = (deviceName, macAddress) => {
     setConnectedDeviceName(deviceName);
     setConnectedMacAddress(macAddress);
     setConnecting(true);
+    setShowToast(true);
+    setToastMessage(`Connecting to ${deviceName}...`);
+    setToastType('');
+  };
+
+  const onClose = () => {
+    setShowToast(false);
+    setToastMessage('');
+    setToastType('');
   };
 
   useEffect(() => {
@@ -77,6 +90,9 @@ export default function Esp32Devices() {
       setAddDeviceName('');
       setAddMacAddress('');
       getDevices();
+      setShowToast(true);
+      setToastMessage(`${addDeviceName} added!`);
+      setToastType('check');
     } catch (error) {
       alert('Error adding device!');
       console.log(error);
@@ -100,6 +116,9 @@ export default function Esp32Devices() {
       const updatedDevices = devices.filter((device) => device.id !== deviceToDelete.id);
       setDevices(updatedDevices);
       getDevices();
+      setShowToast(true);
+      setToastMessage(`${deviceToDelete.device_name} deleted!`);
+      setToastType('warning');
     } catch (error) {
       alert('Error deleting device!');
       console.log(error);
@@ -142,6 +161,9 @@ export default function Esp32Devices() {
       setEditDeviceName('');
       setEditMacAddress('');
       getDevices();
+      setShowToast(true);
+      setToastMessage(`${editDeviceName} updated!`);
+      setToastType('check');
     } catch (error) {
       alert('Error updating device!');
       console.log(error);
@@ -151,114 +173,121 @@ export default function Esp32Devices() {
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.esp32Devices}>
-        <h3>Devices</h3>
-        {editingDeviceIndex === null ? (
-          <ul>
-            {devices.length > 0 ? (
-              devices.map((device, index) => (
-                <li key={index}>
-                  <div className={styles.listItems}>
-                    <p>
-                      {device.device_name}: <span>{device.mac_address}</span>
-                    </p>
-                    <div className={styles.editIcons}>
-                      <div onClick={() => handleDeviceDelete(index)}>
-                        <AiOutlineDelete className="reactIcons" size="1rem" />
-                      </div>
-                      <div onClick={() => handleDeviceEdit(index)}>
-                        <AiOutlineEdit className="reactIcons" size="1rem" />
-                      </div>
-                      <div onClick={() => handleConnect(device.device_name, device.mac_address)}>
-                        <GrConnect className="reactIcons" size="1rem" />
+    <>
+      {showToast && <Toast message={toastMessage} icon={toastType} onClose={onClose} />}
+      <div className={styles.container}>
+        <div className={styles.esp32Devices}>
+          <h2>Devices</h2>
+          {editingDeviceIndex === null ? (
+            <ul>
+              {devices.length > 0 ? (
+                devices.map((device, index) => (
+                  <li key={index}>
+                    <div className={styles.listItems}>
+                      <p>
+                        {device.device_name}: <span>{device.mac_address}</span>
+                      </p>
+                      <div className={styles.editIcons}>
+                        <div onClick={() => handleDeviceDelete(index)}>
+                          <AiOutlineDelete className="reactIcons" size="1rem" />
+                        </div>
+                        <div onClick={() => handleDeviceEdit(index)}>
+                          <AiOutlineEdit className="reactIcons" size="1rem" />
+                        </div>
+                        <div onClick={() => handleConnect(device.device_name, device.mac_address)}>
+                          <GrConnect className="reactIcons" size="1rem" />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </li>
-              ))
-            ) : (
-              <p>No devices added yet.</p>
-            )}
-          </ul>
-        ) : (
-          <div className="w-full pr-6">
-            <Input
-              label="Device Name"
-              color="default"
-              clearable
-              fullWidth
-              bordered
-              borderWeight="light"
-              name="editDeviceName"
-              placeholder={devices[editingDeviceIndex].device_name}
-              value={editDeviceName}
-              onChange={(e) => setEditDeviceName(e.target.value)}
-            />
-            <Spacer y={0.5} />
+                  </li>
+                ))
+              ) : (
+                <p>No devices added yet.</p>
+              )}
+            </ul>
+          ) : (
+            <div className="w-full pr-6">
+              <Input
+                label="Device Name"
+                color="default"
+                clearable
+                fullWidth
+                bordered
+                borderWeight="light"
+                name="editDeviceName"
+                placeholder={devices[editingDeviceIndex].device_name}
+                value={editDeviceName}
+                onChange={(e) => setEditDeviceName(e.target.value)}
+              />
+              <Spacer y={0.5} />
 
-            <Input
-              label="Mac Address"
-              color="default"
-              clearable
-              fullWidth
-              bordered
-              borderWeight="light"
-              name="editMacAddress"
-              placeholder={devices[editingDeviceIndex].mac_address}
-              value={editMacAddress}
-              onChange={(e) => setEditMacAddress(e.target.value)}
-            />
-            <div className="mt-2 flex justify-between">
-              <button
-                className="text-white border-0 w-36 rounded-xl bg-blue-600"
-                onClick={() => handleDeviceUpdate(editingDeviceIndex)}
-              >
-                Update
-              </button>
-              <button
-                className="text-white border-0 w-36 rounded-xl bg-blue-600"
-                bg-blue-600
-                onClick={() => handleDeviceEdit(null)}
-              >
-                Cancel
-              </button>
+              <Input
+                label="Mac Address"
+                color="default"
+                clearable
+                fullWidth
+                bordered
+                borderWeight="light"
+                name="editMacAddress"
+                placeholder={devices[editingDeviceIndex].mac_address}
+                value={editMacAddress}
+                onChange={(e) => setEditMacAddress(e.target.value)}
+              />
+              <div className="mt-2 flex justify-between">
+                <button
+                  className="text-white border-0 w-32 rounded-xl bg-blue-600"
+                  onClick={() => handleDeviceUpdate(editingDeviceIndex)}
+                >
+                  Update
+                </button>
+                <button
+                  className="text-white border-0 w-32 rounded-xl bg-blue-600"
+                  bg-blue-600
+                  onClick={() => handleDeviceEdit(null)}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
+          )}
+        </div>
+        <div className={styles.addDeviceForm}>
+          <h2>Add a new device</h2>
+          <Input
+            label="Device Name"
+            color="default"
+            clearable
+            fullWidth
+            bordered
+            id="deviceName"
+            value={addDeviceName}
+            onChange={(e) => setAddDeviceName(e.target.value)}
+          />
+          <Spacer y={0.5} />
+
+          <Input
+            label="Mac Address"
+            color="default"
+            clearable
+            fullWidth
+            bordered
+            id="macAddress"
+            value={addMacAddress}
+            onChange={(e) => setAddMacAddress(e.target.value)}
+          />
+          <Spacer y={0.5} />
+
+          <div>
+            <button
+              className="text-white border-0 w-full rounded-xl bg-blue-600"
+              onClick={addDevice}
+              disabled={loading}
+            >
+              {loading ? 'Loading ...' : 'Add Device'}
+            </button>
           </div>
-        )}
-      </div>
-      <div className={styles.addDeviceForm}>
-        <h3>Add a new device</h3>
-        <Input
-          label="Device Name"
-          color="default"
-          clearable
-          fullWidth
-          bordered
-          id="deviceName"
-          value={addDeviceName}
-          onChange={(e) => setAddDeviceName(e.target.value)}
-        />
-        <Spacer y={0.5} />
-
-        <Input
-          label="Mac Address"
-          color="default"
-          clearable
-          fullWidth
-          bordered
-          id="macAddress"
-          value={addMacAddress}
-          onChange={(e) => setAddMacAddress(e.target.value)}
-        />
-        <Spacer y={0.5} />
-
-        <div>
-          <Button className="w-full" onClick={addDevice} disabled={loading}>
-            {loading ? 'Loading ...' : 'Add Device'}
-          </Button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
