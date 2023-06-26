563fca6b0aed59736a3a43a2a3675eba8519fe66
shawn <shawnfeng610@gmail.com>
2023-06-26 21:25:17 +0800

    redirect to reset password page

--- webapp/src/pages/_app.jsx
+++ webapp/src/pages/_app.jsx
@@ -3,19 +3,29 @@ import Layout from '@/components/Layout';
 import { AppContextProvider } from '@/contexts/AppContext';
 import nc from 'next-connect';
 import cors from 'cors';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { SessionContextProvider, useUser } from '@supabase/auth-helpers-react';
 import { NextUIProvider } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 function App({ Component, pageProps }) {
   const handler = nc().use(cors());
   const [supabase] = useState(() => createBrowserSupabaseClient());
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      const { query } = router;
+      if (query.type === 'recovery' && query.token) {
+        router.push('/reset-password');
+      }
+    }
+  }, [user, router]);
+
   return (
-    <SessionContextProvider
-      supabaseClient={supabase}
-      initialSession={pageProps.initialSession}
-    >
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <AppContextProvider>
         <NextUIProvider>
           <Layout>
