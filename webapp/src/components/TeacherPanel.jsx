89915e2b2fc9e5cac8c0aa8506e2ab3a2d071a3a
shawn <shawnfeng610@gmail.com>
2023-07-21 16:46:27 +0800

    add Radio for choose route in teacherPanel

--- webapp/src/components/TeacherPanel.jsx
+++ webapp/src/components/TeacherPanel.jsx
@@ -1,8 +1,8 @@
 import { useUser } from '@supabase/auth-helpers-react';
-import { Button, Collapse, Spacer } from '@nextui-org/react';
+import { Button, Collapse, Spacer, Radio } from '@nextui-org/react';
 import { useRouter } from 'next/router';
 import AppContext from '@/contexts/AppContext';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import Toast from './Toast';
 
 export default function TeacherPanel() {
@@ -10,18 +10,22 @@ export default function TeacherPanel() {
   const [toastMessage, setToastMessage] = useState('');
   const [toastType, setToastType] = useState('');
 
+  const user = useUser();
+  const role = user?.user_metadata?.role;
+
+  const { socket, controlMode, setControlMode } = useContext(AppContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log('controlMode: ', controlMode);
+  }, [controlMode]);
+
   const onCloseToast = () => {
     setShowToast(false);
     setToastMessage('');
     setToastType('');
   };
-
-  const user = useUser();
-  const role = user?.user_metadata?.role;
-
-  const { socket } = useContext(AppContext);
-
-  const router = useRouter();
 
   const lockPage = () => {
     socket?.emit('lock_page', router.asPath);
@@ -44,11 +48,33 @@ export default function TeacherPanel() {
         <Collapse
           title="Teacher Panel"
           bordered
-          className="fixed right-0 p-2 mt-20 z-50 text-lg bg-white"
+          className="fixed right-1 px-4 mt-20 z-50 text-lg bg-white"
         >
-          <Button onClick={lockPage}>鎖定頁面</Button>
-          <Spacer y={0.5} />
-          <Button onClick={unlockPage}>解除鎖定</Button>
+          <div className="py-4">
+            <Button onClick={lockPage}>鎖定頁面</Button>
+            <Spacer y={0.5} />
+            <Button onClick={unlockPage}>解除鎖定</Button>
+          </div>
+          <>
+            {router.asPath === '/arm-control' ? (
+              <div className="mt-4 pl-1 pt-3 border border-x-0 border-b-0 border-solid border-slate-300">
+                <Radio.Group
+                  label="連線方式"
+                  Value={controlMode}
+                  defaultValue={controlMode}
+                  onChange={(value) => {
+                    // console.log(value);
+                    setControlMode(value);
+                  }}
+                  className="text-xl"
+                >
+                  <Radio value="single">單一裝置</Radio>
+                  <Radio value="multi-singleRoute">多裝置單一路由</Radio>
+                  <Radio value="multi-multiRoute">多裝置多路由</Radio>
+                </Radio.Group>
+              </div>
+            ) : null}
+          </>
         </Collapse>
       ) : null}
     </>
