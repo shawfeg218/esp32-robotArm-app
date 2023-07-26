ec1a29be9d57e90f8451f0b65cf1b1d2c34cc9ed
shawn <shawnfeng610@gmail.com>
2023-07-26 22:26:29 +0800

    change transcripAudio to backend and fix mobile browser auto play problem in AudioChat.jsx

--- webapp/src/components/audio-chat/AudioChat.jsx
+++ webapp/src/components/audio-chat/AudioChat.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Loading, Input, Button } from '@nextui-org/react';
 import { BsMicFill } from 'react-icons/bs';
 import { IoSend } from 'react-icons/io5';
+import { AiOutlineSound } from 'react-icons/ai';
+import { BiVolumeMute } from 'react-icons/bi';
 import { base64ToBlob } from '../../lib/base64ToBlob';
 import PrettyTextArea from '../PrettyTextArea';
 
@@ -9,9 +11,10 @@ export default function AudioChat() {
   const ansAudioRef = useRef(null);
   const [recording, setRecording] = useState(false);
   const [audioData, setAudioData] = useState(null);
-  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [recorder, setRecorder] = useState(null);
 
   const [ansAudioUrl, setAnsAudioUrl] = useState(null);
+  const [muted, setMuted] = useState(true);
   const [loading, setLoading] = useState(false);
 
   const [text, setText] = useState(null);
@@ -20,32 +23,8 @@ export default function AudioChat() {
   const [conversationHistory, setConversationHistory] = useState([]);
 
   useEffect(() => {
-    let type;
     if (audioData) {
-      switch (audioData.type) {
-        case 'audio/mp3':
-          type = 'audio.mp3';
-          break;
-        case 'audio/mp4':
-          type = 'audio.mp4';
-          break;
-        case 'audio/mpeg':
-          type = 'audio.mpeg';
-          break;
-        case 'audio/mpga':
-          type = 'audio.mpga';
-          break;
-        case 'audio/m4a':
-          type = 'audio.m4a';
-          break;
-        case 'audio/wav':
-          type = 'audio.wav';
-          break;
-        case 'audio/webm':
-          type = 'audio.webm';
-          break;
-      }
-      transcriptAudio(type);
+      transcriptAudio();
     }
   }, [audioData]);
 
@@ -58,110 +37,78 @@ export default function AudioChat() {
   useEffect(() => {
     if (ansAudioUrl) {
       ansAudioRef.current.load();
-    }
-  }, [ansAudioUrl]);
-
-  useEffect(() => {
-    if (ansAudioRef.current) {
       ansAudioRef.current.oncanplaythrough = () => {
         ansAudioRef.current.play();
       };
+      ansAudioRef.current.onerror = (e) => {
+        console.error('Error playing audio:', e);
+      };
     }
-  }, []);
+  }, [ansAudioUrl]);
 
-  async function startRecording() {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
-      const options = [
-        'audio/wav',
-        'audio/m4a',
-        'audio/webm',
-        'audio/mpeg',
-        'audio/mpga',
-        'audio/mp3',
-        'audio/mp4',
-      ];
-      let mimeType;
-
-      for (let i = 0; i < options.length; i++) {
-        if (MediaRecorder.isTypeSupported(options[i])) {
-          mimeType = options[i];
-          console.log('mimeType: ', mimeType);
-          break;
-        }
-      }
-
-      if (mimeType) {
-        const newMediaRecorder = new MediaRecorder(stream, { mimeType });
-        let chunks = [];
-
-        newMediaRecorder.ondataavailable = (e) => {
-          chunks.push(e.data);
-        };
-
-        newMediaRecorder.onstop = (e) => {
-          const audioBlob = new Blob(chunks, { type: mimeType });
-          setAudioData(audioBlob);
-        };
-
-        newMediaRecorder.start();
-        setMediaRecorder(newMediaRecorder);
-        setRecording(true);
-      } else {
-        throw new Error('Your device or browser do not support our recording feature');
-      }
-    } catch (error) {
-      console.log('Error starting recording: ', error);
-      console.log('Error name: ', error.name);
-      console.log('Error message: ', error.message);
-      console.log('Error code: ', error.code);
-      window.alert('Error starting recording');
+  function toggleMute() {
+    if (ansAudioRef.current) {
+      ansAudioRef.current.muted = !ansAudioRef.current.muted;
+      setMuted(!muted);
     }
   }
 
+  async function startRecording() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const recorder = new MediaRecorder(stream);
+
+    let chunks = [];
+
+    recorder.ondataavailable = (e) => {
+      chunks.push(e.data);
+    };
+
+    recorder.onstop = (e) => {
+      const blob = new Blob(chunks);
+      setAudioData(blob);
+      chunks = [];
+    };
+
+    setRecorder(recorder);
+    recorder.start();
+    setRecording(true);
+  }
+
   function stopRecording() {
-    if (mediaRecorder) {
-      mediaRecorder.stop();
+    if (recorder) {
+      recorder.stop();
+      setRecorder(null);
       setRecording(false);
     }
   }
 
-  async function transcriptAudio(type) {
+  async function transcriptAudio() {
     setLoading(true);
-    console.log('type: ', type);
     const formData = new FormData();
-    formData.append('file', audioData, type);
-    formData.append('model', 'whisper-1');
-    formData.append('response_format', 'json');
+    formData.append('file', audioData);
 
-    const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/transcript-audio`, {
       method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_KEY}`,
-      },
       body: formData,
     });
 
     if (!response.ok) {
-      const { error } = await response.json();
+      const errorResponse = await response.json();
       console.log('Error transcribing audio:');
-      console.log('Error message: ', error.message);
-      console.log('Error type: ', error.type);
-      console.log('Error param: ', error.param);
-      console.log('Error code: ', error.code);
+      console.log(errorResponse);
       window.alert('Error transcribing audio');
       setLoading(false);
     } else {
-      const { text } = await response.json();
-      console.log('text: ', text);
-      const newUserMessage = { role: 'user', content: text };
+      const responseJson = await response.json();
+      const responseText = responseJson.text;
+      // console.log(responseText);
+      const newUserMessage = { role: 'user', content: responseText };
       let newConversationHistory = [...conversationHistory, newUserMessage];
       if (newConversationHistory.length > 10) {
         newConversationHistory = newConversationHistory.slice(-10);
       }
       setConversationHistory(newConversationHistory);
-      setText(text);
+      setText(responseText);
     }
   }
 
@@ -185,7 +132,7 @@ export default function AudioChat() {
       setLoading(false);
     } else {
       const responseJson = await response.json();
-      console.log('audioChat: ', responseJson);
+      // console.log('audioChat: ', responseJson);
 
       const { answer, answerAudio } = responseJson;
       const newAssistantMessage = { role: 'assistant', content: answer };
@@ -201,21 +148,29 @@ export default function AudioChat() {
       setAnsAudioUrl(audioUrl);
 
       setLoading(false);
-      console.log('conversationHistory: ', conversationHistory);
+      // console.log('conversationHistory: ', conversationHistory);
     }
   }
 
   return (
     <div className="flex justify-center text-black w-full h-full mt-16">
-      <div className="h-full w-96 max-w-2xl justify-center items-center text-center">
-        <audio ref={ansAudioRef} src={ansAudioUrl} />
+      <div className="h-full w-96 max-w-2xl justify-center items-center text-center relative">
+        <audio autoPlay muted={muted} ref={ansAudioRef} src={ansAudioUrl} />
+        {ansAudioUrl ? (
+          <button
+            className="absolute w-fit right-0 top-2 bg-white flex items-center"
+            onClick={toggleMute}
+          >
+            {muted ? <BiVolumeMute size={32} /> : <AiOutlineSound size={32} />}
+          </button>
+        ) : null}
         <section>
           <h1>Audio chat</h1>
           <div className="overflow-auto h-32 my-4">
             {ans ? <h3>{ans}</h3> : <h3>Start your recording</h3>}
           </div>
-          <div className="w-full flex justify-center">
-            {recording ? <div className="absolute top-80 z-0 spinner"></div> : null}
+          <div className="w-full flex justify-center relative">
+            {recording ? <div className="absolute bottom-2 z-0 spinner"></div> : null}
             <button
               className="bg-transparent w-fit border-0 flex justify-center p-1 z-10"
               onClick={recording ? stopRecording : startRecording}
@@ -226,7 +181,9 @@ export default function AudioChat() {
           <div className="flex justify-center items-center m-8">
             {recording ? <Loading color="currentColor" type="points-opacity" /> : null}
           </div>
-          <h2 className="h-40 overflow-x-scroll">{text ? `${text}` : ''}</h2>
+          <h2 className="h-40 overflow-x-scroll break-words text-center">
+            {text ? `${text}` : ''}
+          </h2>
         </section>
         <section>
           <div className="relative mt-16">
@@ -255,11 +212,11 @@ export default function AudioChat() {
                 </button>
               </div>
             </div>
-            <div>
+            {/* <div>
               {audioData && (
                 <audio controls src={URL.createObjectURL(audioData)} className="w-72" />
               )}
-            </div>
+            </div> */}
           </div>
         </section>
         <div>{loading ? 'Loading... ' : ''}</div>
