3291a8c099961a2aed6dfe52bb57787e600935b3
shawn <shawnfeng610@gmail.com>
2023-08-17 18:15:10 +0800

    modified Home.jsx button

--- webapp/src/components/home/HomeCard.jsx
+++ webapp/src/components/home/HomeCard.jsx
@@ -17,7 +17,7 @@ export default function HomeCard(props) {
           <h1 className="mb-2 tracking-tight text-gray-900">{title}</h1>
           <p>{description}</p>
           <Link href={link} passHref>
-            <button className="w-fit inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+            <button className="w-fit inline-flex justify-center items-center border-0 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
               Get start
               <BsArrowRightShort size={24} />
             </button>
