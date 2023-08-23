a5bca787d81e0cb5c2d51f0427a91c799c03a66b
shawn <shawnfeng610@gmail.com>
2023-08-24 00:08:14 +0800

    fetch paragraph data

--- webapp/src/components/lesson/Textbook.jsx
+++ webapp/src/components/lesson/Textbook.jsx
@@ -1,7 +1,47 @@
 import AppContext from '@/contexts/AppContext';
-import { useState, useContext } from 'react';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useState, useEffect, useContext } from 'react';
 
 export default function Textbook() {
+  const supabase = useSupabaseClient();
+
   const { selectedLesson } = useContext(AppContext);
-  return <div>{selectedLesson}</div>;
+  const [texts, setTexts] = useState([]);
+
+  useEffect(() => {
+    fetchTexts();
+  }, []);
+
+  const fetchTexts = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('lesson_view')
+        .select('*')
+        .eq('lesson_id', selectedLesson)
+        .order('paragraph_order', { ascending: true });
+
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        setTexts(data);
+        console.log('data:', data);
+      }
+    } catch (error) {
+      console.log('Error fetching texts:', error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {texts.map((paragraph) => (
+          <div className="p-2" key={paragraph.paragraph_id}>
+            <p>{paragraph.paragraph_content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
