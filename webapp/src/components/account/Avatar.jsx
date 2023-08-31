1ee7691994d2add77898585ee3974061494a69ac
shawn <shawnfeng610@gmail.com>
2023-08-31 15:17:20 +0800

    modified Avatar loading style and no avatar img style

--- webapp/src/components/account/Avatar.jsx
+++ webapp/src/components/account/Avatar.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
+import { BsFillPersonFill, BsSafe2 } from 'react-icons/bs';
 
 export default function Avatar({ size }) {
   const supabase = useSupabaseClient();
   const user = useUser();
   const [avatar_url, setAvatarUrl] = useState(null);
   const [avatarFileUrl, setAvatarFileUrl] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (user) {
@@ -40,23 +42,26 @@ export default function Avatar({ size }) {
   }
 
   async function downloadImage(path) {
+    setLoading(true);
+
     try {
-      const { data, error } = await supabase.storage
-        .from('avatars')
-        .download(path);
+      const { data, error } = await supabase.storage.from('avatars').download(path);
       if (error) {
         throw error;
       }
       const url = URL.createObjectURL(data);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
       setAvatarFileUrl(url);
     } catch (error) {
       console.log('Error downloading image:', error);
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div>
-      {avatar_url ? (
+      {avatar_url && loading === false ? (
         <img
           src={avatarFileUrl}
           alt="Avatar"
@@ -64,10 +69,11 @@ export default function Avatar({ size }) {
           style={{ height: size, width: size }}
         />
       ) : (
-        <div
-          className="avatar no-image"
-          style={{ height: size, width: size }}
-        />
+        <div className="avatar no-image" style={{ height: size, width: size }}>
+          <div className="h-full flex justify-center items-center">
+            <BsFillPersonFill className="text-slate-400" size={size} />
+          </div>
+        </div>
       )}
     </div>
   );
